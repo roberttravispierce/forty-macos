839b5773996ba014ddb9e49d1f99d15f75620004
Jon Allured <jon.allured@gmail.com>
2017-06-16 08:25:15 -0500

    Initial commit

---
+++ Forty/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Forty
+//
+//  Created by Jonathan Allured on 6/6/17.
+//  Copyright © 2017 Jonathan Allured. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
