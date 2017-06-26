f919ee34e17362f40b26f27bdd20d682a8430c7b
Jon Allured <jon.allured@gmail.com>
2017-06-26 14:06:25 -0500

    Fix tests

--- FortyTests/TotalHoursTests.swift
+++ FortyTests/TotalHoursTests.swift
@@ -3,152 +3,151 @@ import XCTest
 
 class TotalHoursTests: XCTestCase {
     func testNormalDay() {
-        let inTime = "9:00"
-        let outTime = "17:00"
-
-        let totalHours = TotalHours.daily(
-            inTimeString: inTime,
-            outTimeString: outTime,
-            ptoHoursString: "",
-            adjustHoursString: ""
+        let workDay = WorkDay(
+            id: 1,
+            date: Date(),
+            adjustHours: nil,
+            inTime: "9:00",
+            outTime: "17:00",
+            ptoHours: nil
         )
+
+        let totalHours = TotalHours.daily(workDay: workDay)
 
         XCTAssertEqual(totalHours, "8:00")
     }
 
     func testEarlyOut() {
-        let inTime = "9:00"
-        let outTime = "11:00"
-
-        let totalHours = TotalHours.daily(
-            inTimeString: inTime,
-            outTimeString: outTime,
-            ptoHoursString: "",
-            adjustHoursString: ""
+        let workDay = WorkDay(
+            id: 1,
+            date: Date(),
+            adjustHours: nil,
+            inTime: "9:00",
+            outTime: "11:00",
+            ptoHours: nil
         )
+
+        let totalHours = TotalHours.daily(workDay: workDay)
 
         XCTAssertEqual(totalHours, "2:00")
     }
 
     func testLateIn() {
-        let inTime = "15:00"
-        let outTime = "17:00"
-
-        let totalHours = TotalHours.daily(
-            inTimeString: inTime,
-            outTimeString: outTime,
-            ptoHoursString: "",
-            adjustHoursString: ""
+        let workDay = WorkDay(
+            id: 1,
+            date: Date(),
+            adjustHours: nil,
+            inTime: "15:00",
+            outTime: "17:00",
+            ptoHours: nil
         )
+
+        let totalHours = TotalHours.daily(workDay: workDay)
 
         XCTAssertEqual(totalHours, "2:00")
     }
 
     func testInvalidTimes() {
-        let inTime = "asdf"
-        let outTime = "qwer"
-        let ptoTime = "omg"
-        let adjustTime = "lol"
-
-        let totalHours = TotalHours.daily(
-            inTimeString: inTime,
-            outTimeString: outTime,
-            ptoHoursString: ptoTime,
-            adjustHoursString: adjustTime
+        let workDay = WorkDay(
+            id: 1,
+            date: Date(),
+            adjustHours: "lol",
+            inTime: "asdf",
+            outTime: "qwer",
+            ptoHours: "omg"
         )
+
+        let totalHours = TotalHours.daily(workDay: workDay)
 
         XCTAssertEqual(totalHours, "0:00")
     }
 
     func testInvalidOut() {
-        let inTime = "9:00"
-        let outTime = "5:00"
-
-        let totalHours = TotalHours.daily(
-            inTimeString: inTime,
-            outTimeString: outTime,
-            ptoHoursString: "",
-            adjustHoursString: ""
+        let workDay = WorkDay(
+            id: 1,
+            date: Date(),
+            adjustHours: nil,
+            inTime: "9:00",
+            outTime: "5:00",
+            ptoHours: nil
         )
+
+        let totalHours = TotalHours.daily(workDay: workDay)
 
         XCTAssertEqual(totalHours, "0:00")
     }
 
     func testPTO() {
-        let inTime = "10:00"
-        let outTime = "17:00"
-        let ptoTime = "1:00"
-
-        let totalHours = TotalHours.daily(
-            inTimeString: inTime,
-            outTimeString: outTime,
-            ptoHoursString: ptoTime,
-            adjustHoursString: ""
+        let workDay = WorkDay(
+            id: 1,
+            date: Date(),
+            adjustHours: nil,
+            inTime: "10:00",
+            outTime: "17:00",
+            ptoHours: "1:00"
         )
+
+        let totalHours = TotalHours.daily(workDay: workDay)
 
         XCTAssertEqual(totalHours, "8:00")
     }
 
     func testAdjustment() {
-        let inTime = "9:00"
-        let outTime = "17:30"
-        let ptoTime = ""
-        let adjustTime = "0:30"
-
-        let totalHours = TotalHours.daily(
-            inTimeString: inTime,
-            outTimeString: outTime,
-            ptoHoursString: ptoTime,
-            adjustHoursString: adjustTime
+        let workDay = WorkDay(
+            id: 1,
+            date: Date(),
+            adjustHours: "0:30",
+            inTime: "9:00",
+            outTime: "17:30",
+            ptoHours: nil
         )
+
+        let totalHours = TotalHours.daily(workDay: workDay)
 
         XCTAssertEqual(totalHours, "8:00")
     }
 
     func testExtraMinutes() {
-        let inTime = "9:00"
-        let outTime = "19:45"
-        let ptoTime = "0:15"
-        let adjustTime = "2:00"
-
-        let totalHours = TotalHours.daily(
-            inTimeString: inTime,
-            outTimeString: outTime,
-            ptoHoursString: ptoTime,
-            adjustHoursString: adjustTime
+        let workDay = WorkDay(
+            id: 1,
+            date: Date(),
+            adjustHours: "2:00",
+            inTime: "9:00",
+            outTime: "19:45",
+            ptoHours: "0:15"
         )
+
+        let totalHours = TotalHours.daily(workDay: workDay)
 
         XCTAssertEqual(totalHours, "9:00")
     }
 
     func testNegativeMinutes() {
-        let inTime = "8:15"
-        let outTime = "16:00"
-        let ptoTime = "0:30"
-        let adjustTime = "0:45"
-
-        let totalHours = TotalHours.daily(
-            inTimeString: inTime,
-            outTimeString: outTime,
-            ptoHoursString: ptoTime,
-            adjustHoursString: adjustTime
+        let workDay = WorkDay(
+            id: 1,
+            date: Date(),
+            adjustHours: "0:45",
+            inTime: "8:15",
+            outTime: "16:00",
+            ptoHours: "0:30"
         )
+
+        let totalHours = TotalHours.daily(workDay: workDay)
 
         XCTAssertEqual(totalHours, "7:30")
     }
 
     func testVacationDay() {
-        let inTime = ""
-        let outTime = ""
-        let ptoTime = "8:00"
-        let adjustTime = ""
-
-        let totalHours = TotalHours.daily(
-            inTimeString: inTime,
-            outTimeString: outTime,
-            ptoHoursString: ptoTime,
-            adjustHoursString: adjustTime
+        let workDay = WorkDay(
+            id: 1,
+            date: Date(),
+            adjustHours: nil,
+            inTime: nil,
+            outTime: nil,
+            ptoHours: "8:00"
         )
+
+        let totalHours = TotalHours.daily(workDay: workDay)
 
         XCTAssertEqual(totalHours, "8:00")
     }
